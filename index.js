46f827a15e95ae1bd2ab5fb5e4bd4089a7c55967
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-09 10:47:26 -0300

    refactor

--- index.js
+++ index.js
@@ -5,29 +5,6 @@ const cors = require("cors");
 const morgan = require("morgan");
 const Person = require("./models/person");
 
-// let persons = [
-//   {
-//     "id": 1,
-//     "name": "Arto Hellas",
-//     "number": "040-123456"
-//   },
-//   {
-//     "id": 2,
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523"
-//   },
-//   {
-//     "id": 3,
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345"
-//   },
-//   {
-//     "id": 4,
-//     "name": "Mary Poppendieck",
-//     "number": "39-23-6423122"
-//   }
-// ]
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
@@ -74,10 +51,6 @@ app.get("/api/persons/:id", (req, res, next) => {
 
 app.post("/api/persons", (req, res, next) => {
   const { name, number } = req.body;
-  if (!name || !number)
-    return res
-      .status(400)
-      .json({ error: "You must provide a name and a phone number" });
 
   const person = new Person({
     name,
@@ -105,18 +78,13 @@ app.put("/api/persons/:id", (req, res, next) => {
   const { id } = req.params;
   const { name, number } = req.body;
 
-  const person = {
-    name,
-    number,
-  };
-
-  Person.findByIdAndUpdate(id, person, { new: true })
+  Person.findByIdAndUpdate(
+      id,
+      { name, number },
+      { new: true, runValidators: true, context: 'query' }
+    )
     .then((updatedPerson) => {
-      if (updatedPerson) {
         res.json(updatedPerson);
-      } else {
-        res.status(404).end();
-      }
     })
     .catch((error) => next(error));
 });
